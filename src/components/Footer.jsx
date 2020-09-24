21c107a21d49696d3d098751a7fb0e4361c84dc2
Jordan Koehler <digipower66@gmail.com>
2020-09-24 16:44:55 -0400

    init commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const year = new Date();
+const currentYear = year.getFullYear();
+
+function Footer(){
+   return (
+   <footer>
+   <p>Copyright {currentYear}</p>
+   </footer>)
+}
+
+export default Footer
